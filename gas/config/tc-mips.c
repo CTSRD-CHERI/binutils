87b8a0739cd59e8c958ae3ddb644ba6f2eb322a0
Ed Maste <emaste@freebsd.org>
2013-10-22 12:55:26 -0400

    Don't force 64-bit DWARF2 on MIPS
    64-bit debug data is only necessary for objects with greater than 4GB of
    debug data, and is not used on other 64-bit FreeBSD targets.
    
    FreeBSD SVN r256859

--- gas/config/tc-mips.c
+++ gas/config/tc-mips.c
@@ -15192,21 +15192,6 @@ MIPS options:\n\
 #endif
 }
 
-enum dwarf2_format
-mips_dwarf2_format (void)
-{
-  if (mips_abi == N64_ABI)
-    {
-#ifdef TE_IRIX
-      return dwarf2_format_64bit_irix;
-#else
-      return dwarf2_format_64bit;
-#endif
-    }
-  else
-    return dwarf2_format_32bit;
-}
-
 int
 mips_dwarf2_addr_size (void)
 {

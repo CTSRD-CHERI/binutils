d66348e76c9d613aecc29797df73da7c494d183b
Alex Richardson <alexander.richardson@cl.cam.ac.uk>
2016-04-06 11:53:55 +0100

    Add the CHERIBSD files from gnu/usr.bin/binutils

--- ld/emulparams/armelfb_fbsd.sh
+++ ld/emulparams/armelfb_fbsd.sh
@@ -1,4 +1,8 @@
 # $FreeBSD$
+#XXX: This should be used once those bits are merged back in the FSF repo.
+#. ${srcdir}/emulparams/armelf_fbsd.sh
+#
+#OUTPUT_FORMAT="elf32-bigarm"
 . ${srcdir}/emulparams/armelf.sh
 . ${srcdir}/emulparams/elf_fbsd.sh
 TARGET2_TYPE=got-rel
@@ -7,3 +11,4 @@ GENERATE_PIE_SCRIPT=yes
 
 unset STACK_ADDR
 unset EMBEDDED
+OUTPUT_FORMAT="elf32-bigarm"
